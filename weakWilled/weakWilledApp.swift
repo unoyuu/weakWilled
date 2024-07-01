875129e2d60473cbf35eadc4b46cd414648df4b7
unoyu <unosaan@gmail.com>
2024-07-02 08:30:14 +0900

    Initial Commit

---
+++ weakWilled/weakWilledApp.swift
@@ -0,0 +1,17 @@
+//
+//  weakWilledApp.swift
+//  weakWilled
+//
+//  Created by 宇野佑 on 2024/07/02.
+//
+
+import SwiftUI
+
+@main
+struct weakWilledApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
